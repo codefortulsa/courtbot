3161145eab0d5378f0148b6a980a67c5cd6336e3
Philip Kin <philip@thekinfamily.com>
2016-12-13 10:38:24 -0600

    More correct twillio

--- sms/registerRoutes.js
+++ sms/registerRoutes.js
@@ -19,20 +19,29 @@ module.exports = function(req, res, next) {
     console.log("YES - Route");
     registration.confirmReminders(phone, true, twiml)
       .catch(err => twiml.sms(err))
-      .then(() => res.send(twiml.toString()));
+      .then(() => {
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString())
+      });
   }
   else if(isResponseNo(text)) {
     console.log("NO - Route");
     registration.confirmReminders(phone, false, twiml)
       .catch(err => twiml.sms(err))
-      .then(() => res.send(twiml.toString()));
+      .then(() => {
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString())
+      });
   }
   else if(!isNaN(parseFloat(text)) && isFinite(text)) {
     console.log("NUMBER - Route");
     registration.selectParty(phone, parseInt(text), twiml)
       .catch(err => registration.beginRegistration(text, phone, twiml))
       .catch(err => twiml.sms(err))
-      .then(() => res.send(twiml.toString()));
+      .then(() => {
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString())
+      });
   }
   else {
     console.log("CASE NUMBER - Route - " + text + " - " + phone);
@@ -41,6 +50,9 @@ module.exports = function(req, res, next) {
         console.log("ERROR - " + err)
         twiml.sms(err);
       })
-      .then(() => res.send(twiml.toString()));
+      .then(() => {
+        res.writeHead(200, {'Content-Type': 'text/xml'});
+        res.end(twiml.toString())
+      });
   }
 }
