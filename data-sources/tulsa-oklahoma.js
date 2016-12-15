18a0d1b0e495075c98a76d41e1a025e7c0650182
Philip Kin <philip@thekinfamily.com>
2016-12-14 18:16:18 -0600

    Attach to oscn api

--- data-sources/tulsa-oklahoma.js
+++ data-sources/tulsa-oklahoma.js
@@ -1,13 +1,27 @@
+var Client = require('node-rest-client').Client;
+require('../config');
+
+var client = new Client();
 
 module.exports.getCaseParties = function(casenumber) {
   return new Promise(function(resolve, reject) {
-    resolve([{name: "Test"}, {name: "Test2"}]);
+    client.get("http://data.thekinfamily.com/oscn/case/tulsa/" + casenumber, function(data, res) {
+      if(!data.parties) {
+        reject("no parties");
+      }
+      resolve(data.parties);
+    });
   });
 }
 
 module.exports.getCasePartyEvents = function(casenumber, partyName) {
   return new Promise(function(resolve, reject) {
-    resolve([{date:"2016-12-14", description:"YOU GOTS A THING"}]);
+    client.get("http://data.thekinfamily.com/oscn/case/tulsa/" + casenumber + "/" + partyName, function(data, res) {
+      if(!data.events) {
+        reject("no events");
+      }
+      resolve(data.events);
+    });
   });
 }
 
